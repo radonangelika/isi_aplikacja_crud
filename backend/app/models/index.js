485d1b28c54d1f32f24be0d981a337634ba01349
radonangelika <radonangelika1@gmail.com>
2025-06-11 17:25:54 +0200

    Update index.js

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,23 +1,23 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql"
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.Klientka = require("./klientka.model.js")(sequelize, DataTypes);
-
-
-db.authenticate = () => sequelize.authenticate().then(() => console.log("✅ Połączono z bazą"));
-db.sync = () => sequelize.sync();
 db.User = require("./user.model.js")(sequelize, DataTypes);
+
+db.authenticate = () => sequelize.authenticate().then(() => console.log("✅ Połączono z PostgreSQL"));
+db.sync = () => sequelize.sync();
+
 module.exports = db;
